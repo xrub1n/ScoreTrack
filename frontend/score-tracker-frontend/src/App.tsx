e3483df98f7de4ed33fddf764e02ec0a53c07a41
Joshua Rubin-Garcia <jrr2386@rit.edu>
2025-10-07 15:12:32 -0400

    Created React template using Typescript, created empty files for all components and pages necessary

---
+++ frontend/score-tracker-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>ScoreTrack</h1>
+      <p>Frontend connected to backend coming soon!</p>
+    </div>
+  );
+}
+
+export default App;
